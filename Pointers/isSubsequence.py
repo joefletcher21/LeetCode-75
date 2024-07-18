6b3e12883b0de902db702d9b78ed7c43c0ffefae
Joseph Fletcher <jckmfletcher@gmail.com>
2024-07-18 12:33:40 -0400

    Update isSubsequence.py
    Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?

--- Pointers/isSubsequence.py
+++ Pointers/isSubsequence.py
@@ -1,13 +1,23 @@
 class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
+    subsequences = ["ace", "one", "prime"]
+    targetStr = "cdnckjdncwcwwckkcfvjkrvbvbnvliasbvnfdbgnjoihrtiyureoqfjnbjbnrlkwwpxmnmcncmbvnrmxvnvifkmznsmesfnjngreiunojlsdkmvnsedme"
+    results = []
+
+    @staticmethod
+    def isSubsequence(s: str, t: str) -> bool:
         if len(s) > len(t):
             return False
-        elif s == t  or s == "":
+        elif s == t or s == "":
             return True
         else:
-            count=0
+            count = 0
             for i in range(len(t)):
-                if count <= len(s)-1:
+                if count <= len(s) - 1:
                     if s[count] == t[i]:
-                        count+=1
+                        count += 1
             return count == len(s)
+
+    for s in subsequences:
+        results.append(isSubsequence(s, targetStr))
+
+    print(results)
