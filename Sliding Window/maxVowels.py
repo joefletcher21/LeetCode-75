73a70fa671b194c2d8e734ac605de9e60a7f3fad
Joseph Fletcher <jckmfletcher@gmail.com>
2024-07-22 10:25:55 -0400

    Update maxVowels.py
    Passes all tests

--- Sliding Window/maxVowels.py
+++ Sliding Window/maxVowels.py
@@ -1,16 +1,13 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
-        currCount = maxCount = 0
-        i = 0
-        firstVowelIndex = len(s)
-        for char in "aeiou":
-            if s.find(char) != -1:
-                firstVowelIndex = min(firstVowelIndex,s.find(char))
-        if firstVowelIndex == len(s):
-            return 0
-        while i <= len(s)-k:
-            substring = s[i:i+k]
-            currCount = sum(1 for char in substring if char in "aeiou")
+        
+        substring = s[:k]
+        currCount = maxCount = sum(1 for char in substring if char in "aeiou")
+        
+        for i in range(k,len(s)):
+            if s[i-k] in "aeiou":
+                currCount -= 1
+            if s[i] in "aeiou":
+                currCount += 1
             maxCount = max(currCount,maxCount)
-            i+=1
         return maxCount
